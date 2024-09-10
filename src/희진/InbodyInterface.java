3017cc6c990797b76cc4030097274851a9a6d4fd
heejin <jamppohj@naver.com>
2024-09-10 13:06:46 +0900

    update 삭제한 코드

--- src/희진/InbodyInterface.java
+++ src/희진/InbodyInterface.java
@@ -2,15 +2,13 @@ package 희진;
 
 import java.util.List;
 
-public interface TrainerInterface {
+public interface InbodyInterface {
 	
-	boolean insert(Inbody inbody); // 새회원 인바디 입력
-
-	boolean update(Inbody inbody); // 변경된 인바디 저장 필요
+	boolean insert(Inbody inbody); 
 	
-	boolean deleteBybodynum(double Inbody_num); // 회원 탈퇴한 사람의 정보 삭제
+	boolean deleteBybodynum(int Inbody_num);
 	
-	public List<Inbody> findAll();	// 모든 회원의 정보를 볼 필요는 없음
+	public List<Inbody> findAll();
 	
-	public Inbody findBybodynum(double Inbody_num); // 회원이 본인의 정보 검색 . 그러나 멤버 클래스에서 필요	
+	public Inbody findBybodynum(int Inbody_num);	
 }
--- src/희진/JDBCInbodyDao.java
+++ src/희진/JDBCInbodyDao.java
@@ -7,19 +7,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JDBCTrainerDao implements TrainerInterface {
+public class JDBCInbodyDao implements InbodyInterface {
 
 	@Override
 	public boolean insert(Inbody inbody) {
 		boolean result = false;
-		
-		String sql = "INSERT INTO "
-				+ "INBODY(M_DATE, WEIGHT, HEIGHT, FAT, MUSCLE, BODY_AGE, BODY_SCORE, MEMBER_NUM) "
+
+		String sql = "INSERT INTO " + "INBODY(M_DATE, WEIGHT, HEIGHT, FAT, MUSCLE, BODY_AGE, BODY_SCORE, MEMBER_NUM) "
 				+ "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)";
-		
+
 		try (Connection connection = DataSource.getDataSource();
-	             PreparedStatement pStatement = connection.prepareStatement(sql)) {
-			
+				PreparedStatement pStatement = connection.prepareStatement(sql)) {
+
 			pStatement.setTimestamp(1, inbody.getM_date());
 			pStatement.setDouble(2, inbody.getWeight());
 			pStatement.setDouble(3, inbody.getHeight());
@@ -28,9 +27,9 @@ public class JDBCTrainerDao implements TrainerInterface {
 			pStatement.setInt(6, inbody.getBody_age());
 			pStatement.setInt(7, inbody.getBody_score());
 			pStatement.setInt(8, inbody.getMember_num());
-			
+
 			pStatement.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -38,67 +37,34 @@ public class JDBCTrainerDao implements TrainerInterface {
 	}
 
 	@Override
-	public boolean update(Inbody inbody) {
-		
-		boolean result = false;
-		
-		String sql = "UPDATE INBODY "
-				+ "SET m_date = ?, weight = ?, height = ?, fat = ?, muscle = ?, body_age = ?, body_score = ?, member_num = ? "
-				+ "WHERE inbody_num = ?";
-		
-		try (Connection connection = DataSource.getDataSource();
-             PreparedStatement pStatement = connection.prepareStatement(sql)) {
+	public boolean deleteBybodynum(int Inbody_num) {
 
-            pStatement.setTimestamp(1, inbody.getM_date());
-            pStatement.setDouble(2, inbody.getWeight());
-            pStatement.setDouble(3, inbody.getHeight());
-            pStatement.setDouble(4, inbody.getFat());
-            pStatement.setDouble(5, inbody.getMuscle());
-            pStatement.setInt(6, inbody.getBody_age());
-            pStatement.setInt(7, inbody.getBody_score());
-            pStatement.setInt(8, inbody.getMember_num());
-            pStatement.setInt(9, inbody.getInbody_Num());
-            
-            int rows = pStatement.executeUpdate();
-            System.out.println(inbody);
-            System.out.println(rows);
-            if(rows > 0) {
-				result = true;
-			}
-		} catch (SQLException e) {
-            e.printStackTrace();
-        } 	return result;
-	}
-
-	@Override
-	public boolean deleteBybodynum(double Inbody_Num) {
-		
 		String sql = "DELETE FROM INBODY WHERE INBODY_NUM = ?";
-		
+
 		try (Connection connection = DataSource.getDataSource();
-	             PreparedStatement pStatement = connection.prepareStatement(sql)) {
-			
-			pStatement.setDouble(1, Inbody_Num);
-			
+				PreparedStatement pStatement = connection.prepareStatement(sql)) {
+
+			pStatement.setInt(1, Inbody_num);
+
 			int rows = pStatement.executeUpdate();
-            return rows > 0;
-            
+			return rows > 0;
+
 		} catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+			e.printStackTrace();
+			return false;
+		}
 	}
-	
+
 	public List<Inbody> findAll() {
-		
+
 		List<Inbody> inbodys = new ArrayList<>();
 		String sql = "SELECT * FROM INBODY ORDER BY INBODY_NUM DESC";
-		
+
 		try (Connection connection = DataSource.getDataSource();
-	            PreparedStatement pStatement = connection.prepareStatement(sql);
+				PreparedStatement pStatement = connection.prepareStatement(sql);
 				ResultSet rs = pStatement.executeQuery()) {
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				Inbody inbody = new Inbody();
 				inbody.setInbody_Num(rs.getInt("inbody_num"));
 				inbody.setM_date(rs.getTimestamp("m_date"));
@@ -109,45 +75,43 @@ public class JDBCTrainerDao implements TrainerInterface {
 				inbody.setBody_age(rs.getInt("body_age"));
 				inbody.setBody_score(rs.getInt("body_score"));
 				inbody.setMember_num(rs.getInt("member_num"));
-				
+
 				inbodys.add(inbody);
 			}
-			} catch (SQLException e) {
-	            e.printStackTrace();
-	            return inbodys;
-	        }
-		return inbodys;
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-	
-	public Inbody findBybodynum(double Inbody_Num) {
-		
+		return inbodys;
+	}
+
+	public Inbody findBybodynum(int Inbody_num) {
+
 		Inbody inbody = null;
 		String sql = "SELECT * FROM INBODY WHERE INBODY_NUM = ?";
-		
+
 		try (Connection connection = DataSource.getDataSource();
-	            PreparedStatement pStatement = connection.prepareStatement(sql)){
-			
-			pStatement.setDouble(1, Inbody_Num);
-			
- 		try (ResultSet rs = pStatement.executeQuery()){
-			
-			if(rs.next()){
-				inbody = new Inbody();
-				inbody.setInbody_Num(rs.getInt("Inbody_Num"));
-				inbody.setM_date(rs.getTimestamp("M_date"));
-				inbody.setWeight(rs.getDouble("Weight"));
-				inbody.setHeight(rs.getDouble("Height"));
-				inbody.setFat(rs.getDouble("Fat"));
-				inbody.setMuscle(rs.getDouble("Muscle"));
-				inbody.setBody_age(rs.getInt("Body_age"));
-				inbody.setBody_score(rs.getInt("Body_score"));
-				inbody.setMember_num(rs.getInt("Member_num"));
+				PreparedStatement pStatement = connection.prepareStatement(sql)) {
+
+			pStatement.setInt(1, Inbody_num);
+
+			try (ResultSet rs = pStatement.executeQuery()) {
+
+				if (rs.next()) {
+					inbody = new Inbody();
+					inbody.setInbody_Num(rs.getInt("Inbody_Num"));
+					inbody.setM_date(rs.getTimestamp("M_date"));
+					inbody.setWeight(rs.getDouble("Weight"));
+					inbody.setHeight(rs.getDouble("Height"));
+					inbody.setFat(rs.getDouble("Fat"));
+					inbody.setMuscle(rs.getDouble("Muscle"));
+					inbody.setBody_age(rs.getInt("Body_age"));
+					inbody.setBody_score(rs.getInt("Body_score"));
+					inbody.setMember_num(rs.getInt("Member_num"));
+				}
 			}
-			} 
 		} catch (SQLException e) {
-        e.printStackTrace();
-		} 
+			e.printStackTrace();
+		}
 		return inbody;
-    
 	}
 }
