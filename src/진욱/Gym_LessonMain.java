0fef472b13afe23bf50907e47130fc51e254a15a
Joyfuler <137759165+Joyfuler@users.noreply.github.com>
2024-09-19 19:17:29 +0900

    Update Gym_LessonMain.java
    local change

--- src/진욱/Gym_LessonMain.java
+++ src/진욱/Gym_LessonMain.java
@@ -15,14 +15,18 @@ public class Gym_LessonMain {
     ReviewDao rDao = DAOManager.getInstance().getrDao();
 
     public void lessonExecute(){
+
         System.out.println("수업 메뉴에서 원하는 기능을 선택해주세요.");
+
         boolean loop = true;
         while (loop){
             ShowManager.getInstance().showLessonMenu();
             int select = Input.intScan();
             switch (select){
                 case 1:
+
                     System.out.println("전체 수업 내용을 출력합니다.");
+
                     for (Gym_Lesson gl : glDao.findAll()){
                         System.out.println(gl);
                     }
@@ -38,11 +42,15 @@ public class Gym_LessonMain {
                     deleteLesson();
                     break;
                 case 9:
+
                     System.out.println("초기 화면으로 돌아갑니다.");
+
                     loop = false;
                     break;
                 default:
+
                     System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+
                     break;
             }
         }
@@ -65,26 +73,35 @@ public class Gym_LessonMain {
         for (Gym_Lesson gl3: glDao.findAll()){
             System.out.println(gl3);
         }
+
         System.out.println("수정을 원하는 수강정보의 번호를 입력해주세요.");
         int number = Input.intScan();
+
         System.out.println("선택하신 수강정보는 아래와 같습니다.");
+
         System.out.println((glDao.getALesson(number) == null? "수강 정보가 없습니다!" : glDao.getALesson(number)));
+
         System.out.println("수정할 수강 과목명을 입력해주세요. ex) 다이어트 집중 수업");
         String class_detail = Input.stringScan();
+
         for (Trainer t : tDao.findAll()){ // 번호 입력 전 트레이너 전체 목록을 출력함.
             System.out.println(t);
         }
+
         System.out.println("수정할 담당 트레이너가 누구인지 번호를 입력해주세요.");
         int trainer_num = Input.intScan();
+
         Trainer modifyTrainer = tDao.findByIndex(trainer_num);
         for (Gym_Member m : gmDao.findAll()){
             System.out.println(m);
         }
+
         System.out.println("어떤 회원으로 수정할 지 번호를 입력해주세요. (회원 목록 출력)");
         int member_num = Input.intScan();
-        Gym_Member modifyMember = gmDao.findByMember_Num(member_num);
 
+        Gym_Member modifyMember = gmDao.findByMember_Num(member_num);
         int result = glDao.updateLesson(new Gym_Lesson(number, class_detail, modifyTrainer, modifyMember));
+
         System.out.println(result + "건 변경이 완료되었습니다.");
     }
 
@@ -95,8 +112,10 @@ public class Gym_LessonMain {
 
         System.out.println("삭제하고자 하는 강의의 번호를 입력해주세요. (삭제 시 관련 리뷰도 함께 삭제됩니다.");
         int number = Input.intScan();
+
         int reviewDeleteResult = rDao.deleteReviewByClassNumber(number);
         int classDeleteResult = glDao.deleteLesson(number);
+
         if (reviewDeleteResult != 0 && classDeleteResult != 0) {
             System.out.println("해당 강의 정보 및 그 리뷰가 삭제되었습니다.");
         }
